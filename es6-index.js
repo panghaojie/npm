4435f0285bb2e2958dad5c9cb3b8de873273b8d3
panghaojie <pang_hao_jie@163.com>
2019-11-03 23:58:21 +0800

    publish

---
+++ es6-index.js
@@ -0,0 +1,7 @@
+import util from './lib/phj-util'
+const {
+    qrcode
+} = util
+export default {
+    qrcode
+}
---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _phjUtil = require('./lib/phj-util');
+
+var _phjUtil2 = _interopRequireDefault(_phjUtil);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var qrcode = _phjUtil2.default.qrcode;
+exports.default = {
+    qrcode: qrcode
+};
